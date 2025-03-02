0e59d254ce672a9476c41cd8f584005ccb0ecc05
Bishallk <3276617@mbmc.edu.np>
2025-03-02 16:43:40 +0545

    opetators in python

---
+++ Operators/Operators.py
@@ -0,0 +1,48 @@
+
+#---------Arithmetic Operators----------|
+
+x,y=5,2
+
+print(x+y) #addition
+print(x-y) # subtraction
+print(x*y) #multiply
+print(x/y) #true div
+print(x//y) #int div
+print(x%y) #modulus
+print(x**y) #power of
+
+
+#---------Comparision Operators----------|
+
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+print(x==y)
+print(x!=y)
+
+#---------Logical Operators----------|
+x=True
+y=False
+
+print(x or y)
+print(x and y)
+print(not y)
+
+#---------Bitwise Operators----------|
+#* works on binary values
+a,b=2,3
+print(x & y) # bitwise and
+print(x | y) # bitwise or
+print(x >> y) # right shift
+print(x << y) # left shift
+print(~a) #1's complement
+
+
+#---------Assinment Operators----------|
+
+c=3
+c+=3 #c=c+3
+c-=3 #c=c-3
+c*=3 #c=c*3
+
